19a5c93827ed233a02413f39d29713c565ce88ad
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-03-27 22:13:37 -0400

    work needs to be done on test class and children classes

---
+++ src/TestNetflix.java
@@ -0,0 +1,11 @@
+public class TestNetflix {
+
+  // TODO: Need to test:
+  //  - NetflixTitle parent class
+  //    - Movies child class
+  //    - Shows child class
+
+  public static void main(String[] args) {
+     
+  }
+}
