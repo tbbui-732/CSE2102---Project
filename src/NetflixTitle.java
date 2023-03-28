5c109b1115f706aad2d2ba0bc30a58ee418eb0d7
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-03-27 22:04:31 -0400

    added mutator methods, not tested yet

--- src/NetflixTitle.java
+++ src/NetflixTitle.java
@@ -26,15 +26,72 @@ public class NetflixTitle {
   }
 
   public void addCountry(String newCountry) {
+    // Create a new array for countries
+    int n = countries.length; 
+    String newCountries[] = new String[n+1];
 
+    // Copy values of old array to new array
+    for (int i = 0; i < n; i++) {
+      newCountries[i] = countries[i];
+    }
+
+    // Add new country
+    newCountries[n] = newCountry;
+    countries = newCountries;
   }
 
   public void removeCountry(String countryToRemove) {
+    // Make sure countryToRemove exists
+    int n = countries.length;
+    boolean exists = false;
+    for (int i = 0; i < n; i++) {
+      if (countries[i] == countryToRemove) {
+        exists = true;
+        break;
+      }
+    }
 
+    if (!exists) {
+      System.out.println("The attribute supplied does not exist"); 
+      return;
+    }
+
+    // Create a new array for countries
+    String newCountries[] = new String[n-1];
+
+    // Copy values except the country to be removed
+    int new_i = 0;
+    int old_i = 0;
+
+    while (old_i < n) {
+      if (countries[old_i] != countryToRemove) {
+        newCountries[new_i] = countries[old_i];
+        new_i++;
+        old_i++;
+      }
+      else {
+        old_i++;
+      }
+    }
+
+    // Set country to newCountries
+    countries = newCountries;
   }
 
   public void setNewCountries(String newCountries[]) {
+    countries = newCountries;
+  }
 
+  public void changeGenre(String newGenre) {
+    genre = newGenre;
+  }
+
+  public void changeShowID(String newID) {
+    show_id = newID;
+  }
+
+  public void changeRating(String newRating) {
+    rating = newRating;
   }
 
   // Accessor methods
