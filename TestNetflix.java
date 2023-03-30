d009acf3e5f5b0a969a4d7562a6be73e24d6113b
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-03-29 22:20:39 -0400

    wrote build method

--- TestNetflix.java
+++ TestNetflix.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class TestNetflix {
-
-  // TODO: Need to test:
-  //  - NetflixTitle parent class
-  //    - Movies child class
-  //    - Shows child class
-
   
+  public static int buildNetflixTitle() {
+    // Returns 0 for MOVIES and 1 for TV SHOW
+    // Return -1 for bad answer
+
+    // Get user input
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Would you like to build a TV Show or a Movie?");
+    String selection = sc.nextLine();
+    sc.close();
+
+    // Build based on selection
+    System.out.printf("Okay, we are going to build a %s\n", selection);
+
+    if (selection == "TV Show") {
+      return 1;
+    } else if (selection == "Movie") {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
 
   public static void main(String[] args) {
-      
+    buildNetflixTitle();
   }
 }
