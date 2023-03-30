62ec91b04644854583760807535b3a03a1245788
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-03-29 23:22:38 -0400

    added display show information in netflix containers

--- NetflixTitleContainers.java
+++ NetflixTitleContainers.java
@@ -44,10 +44,53 @@ public class NetflixTitleContainers {
     return 0;
   }
 
+  public static int displayShowInfo(String showName) {
+    // Return 1 if successful
+    //        0 otherwise
+
+    int n = showsContainer.length;
+    
+    // Return error if empty
+    if (n == 0) {
+      System.out.println("Shows database is currently empty");
+      return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (showName == showsContainer[i].getTitle()) {
+        // Store information
+        String title      = showsContainer[i].getTitle();
+        String director   = showsContainer[i].getDirector();
+        String country    = showsContainer[i].getCountry();
+        String genre      = showsContainer[i].getGenre();
+        String show_id    = showsContainer[i].getID();
+        String rating     = showsContainer[i].getRating();
+        int numSeasons    = showsContainer[i].getNumSeasons();
+        int numEpisodes   = showsContainer[i].getNumEpisodes();
+
+        // Display information
+        System.out.printf("Title: %s\n",              title);
+        System.out.printf("Director: %s\n",           director);
+        System.out.printf("Country: %s\n",            country);
+        System.out.printf("Genre: %s\n",              genre);
+        System.out.printf("Show ID: %s\n",            show_id);
+        System.out.printf("Rating: %s\n",             rating);
+        System.out.printf("Number of seasons: %d\n",  numSeasons);
+        System.out.printf("Number of episodes: %d\n", numEpisodes);
+
+        return 1;
+      }
+    }
+
+    // Show does not exist in database
+    System.out.printf("The show '%s' does not exist in the database\n", showName);
+    return 0;
+  }
+
   // Mutator methods
   public static void addMovie(NetflixMovies movie) {
     int n = moviesContainer.length;
-    NetflixMovies newMoviesContainer[] = NetflixMovies[n+1];
+    NetflixMovies newMoviesContainer[] = new NetflixMovies[n+1];
 
     for (int i = 0; i < n; i++) {
       newMoviesContainer[i] = moviesContainer[i];
