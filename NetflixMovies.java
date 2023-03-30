79c9c57d417713b197400d2a9a8ad6e60b94a557
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-03-29 23:21:24 -0400

    changed country to String instead of String array

--- NetflixMovies.java
+++ NetflixMovies.java
@@ -2,13 +2,13 @@ public class NetflixMovies extends NetflixTitle {
   private int duration;
   public NetflixMovies(String initTitle,
                       String initDirector,
-                      String initCountries[],
+                      String initCountry,
                       String initGenre,
                       String initShowID,
                       String initRating,
                       int initDuration) 
   {
-    super(initTitle, initDirector, initCountries, initGenre, initShowID, initRating);
+    super(initTitle, initDirector, initCountry, initGenre, initShowID, initRating);
     duration = initDuration;
   }
 
