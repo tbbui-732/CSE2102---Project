0e5cefc40cb866c7c39fdbbf3576afe70a1597f8
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-04-04 17:02:44 -0400

    Wrote method to parse CSV files to display

---
+++ CSVParser.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class CSVParser {
+    private String filename;
+    
+    public CSVParser(String filename) {
+        this.filename = filename;
+    }
+
+    public void displayCSVFile() {
+        try {
+            Scanner sc = new Scanner(new File(filename));
+            sc.useDelimiter(",");
+            while (sc.hasNext()) {
+                System.out.print(sc.next());
+            }
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
